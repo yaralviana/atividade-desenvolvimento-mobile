33a404f472dbf3b4cf115b5d45b0b9c4af2169d0
Yara <yaralviana@gmail.com>
2023-04-24 23:07:05 -0300

    Ajustes e validação de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
+ 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
 })
-export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class LoginPage {
+  username?: string;
+  password?: string;
+ 
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+ 
   }
-
+ 
+  login() {
+    //simulando um usuário e senha
+    if (this.username === 'usertest' && this.password === '123456') {
+      this.navCtrl.navigateForward('/welcome');
+      this.showWelcomeAlert();
+    } else {
+      this.showInvalidCredentialsAlert();
+    }
+  }
+ 
+  async showWelcomeAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Bem-vindo',
+      message: `Olá ${this.username}!`,
+      buttons: ['OK']
+    });
+ 
+    await alert.present();
+  }
+ 
+  async showInvalidCredentialsAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Erro',
+      message: 'Usuário ou senha inválidos.',
+      buttons: ['OK']
+    });
+ 
+    await alert.present();
+  }
 }
--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+ 
 @Component({
   selector: 'app-welcome',
-  templateUrl: './welcome.page.html',
-  styleUrls: ['./welcome.page.scss'],
+  templateUrl: 'welcome.page.html',
+  styleUrls: ['welcome.page.scss'],
 })
-export class WelcomePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class WelcomePage {
+  username: string;
+ 
+  constructor(private route: ActivatedRoute) {
+    this.username = this.route.snapshot.paramMap.get('username')!;
   }
-
 }
